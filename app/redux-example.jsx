fe8c200b60cb933eef78f13563506d547802bb27
davfry <davecfryer@gmail.com>
2017-03-20 22:14:45 -0600

    add devtools

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -5,9 +5,40 @@ console.log('starting redux example');
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous';
 
-  return state;
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      default:
+        return state;
+  }
 };
-var store =  redux.createStore(reducer);
+var store =  redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension(): f => f
+));
+
+//Subscribe
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('Name is ', state.name);
+  document.getElementById('app').innerHTML = state.name;
+});
+// unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState: ', currentState);
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Dave'
+});
+
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Chaz'
+});
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -9,10 +9,39 @@ var stateDefault = {
 };
 
 var reducer = (state = stateDefault, action) => {
-    return state;
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+      default:
+        return state;
+      }
   };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension(): f => f
+));
+
+store.subscribe(() => {
+  var state = store.getState();
+
+  document.getElementById('app').innerHTML = state.searchText;
+})
 
 var currentState = store.getState();
 console.log('currentState: ', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'New search text'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'New search text 2'
+});
+
+console.log('Should be new search text', store.getState() );
